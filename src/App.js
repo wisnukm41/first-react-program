ed7708f68dab989f34bb7778b7d979aff7e8b307
Wisnu Murfadilah <wisnukm41@gmail.com>
2023-01-30 16:01:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function Form(props) {
+  const [value, setValue] = useState("");
+
+  function onChange(e) {
+    setValue(e.target.value);
+  }
+
+  function onSubmit(e) {
+    if (value !== "") {
+      props.submit(value);
+      setValue("");
+    }
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          placeholder="What is in your mind?"
+          value={value}
+        />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+}
+
+function ListValue(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) => <li key={index}>{val}</li>);
+
+  return (
+    <div>
+      <ul>{listItems}</ul>
+    </div>
+  );
+}
 
 function App() {
+  const [values, setValues] = useState(["James"]);
+
+  function add(val) {
+    setValues([...values, val]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Form submit={add} />
+      <ListValue data={values} />
     </div>
   );
 }
